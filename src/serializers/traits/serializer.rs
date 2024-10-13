35968eb9b371c3fb76958f38131e6dcea875e32c
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2024-10-13 19:22:28 +0200

    Rollback having blanket impl for serializer ref

--- src/serializers/traits/serializer.rs
+++ src/serializers/traits/serializer.rs
@@ -25,22 +25,6 @@ impl<S: PacketSerializer> IntoPacketSerializer for S {
     }
 }
 
-impl<'a, S: ?Sized + PacketSerializer> PacketSerializer for &'a S {
-    type SerializedPacket = S::SerializedPacket;
-    type SerializeError = S::SerializeError;
-
-    type DeserializedPacket = S::DeserializedPacket;
-    type DeserializeError = S::DeserializeError;
-
-    fn serialize(&self, packet: &Self::SerializedPacket) -> Result<Vec<u8>, Self::SerializeError> {
-        (*self).serialize(packet)
-    }
-
-    fn deserialize(&self, data: Cow<'_, [u8]>) -> Result<Self::DeserializedPacket, Self::DeserializeError> {
-        (*self).deserialize(data)
-    }
-}
-
 impl<S: ?Sized + PacketSerializer> PacketSerializer for Box<S> {
     type SerializedPacket = S::SerializedPacket;
     type SerializeError = S::SerializeError;
